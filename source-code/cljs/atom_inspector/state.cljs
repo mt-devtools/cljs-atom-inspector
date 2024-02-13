d4f4e2f7fd01fb1eed3352161f7fe1bec46e6029
Bit Handshake <adam@monotech.hu>
2024-02-13 23:41:20 +0100

    0.1.3.1

--- source-code/cljs/atom_inspector/state.cljs
+++ source-code/cljs/atom_inspector/state.cljs
@@ -1,6 +1,6 @@
 
 (ns atom-inspector.state
-    (:require [reagent.core :refer [atom] :rename {atom ratom}]))
+    (:require [reagent.core :as reagent]))
 
 ;; ----------------------------------------------------------------------------
 ;; ----------------------------------------------------------------------------
@@ -10,4 +10,4 @@
 ; @atom (map)
 ; - The inspector stores the reference of the inspected atom and the meta items in the 'INSPECTORS' atom.
 ; - Its keyed by the inspector IDs to provide the ability of multiple use of the inspector.
-(defonce INSPECTORS (ratom {}))
+(defonce INSPECTORS (reagent/atom {}))
